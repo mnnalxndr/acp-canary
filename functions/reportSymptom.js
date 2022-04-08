5f9bed43796a283083b23c721c6b7c7f2d52df34
Alex.Mann <alex.mann@theaccessgroup.com>
2022-04-08 15:02:30 +0100

    Testing reportSymptom 3

--- functions/reportSymptom.js
+++ functions/reportSymptom.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  const [ userId, symptomId, serviceUserId ] = req.query;
+  // const [ userId, symptomId, serviceUserId ] = req.query;
   // res.status(200).send(`User: ${userId} - Reporting symptom ${symptomId} - For service user ${serviceUserId}!`);
   res.status(200).send(`${JSON.stringify(req)}`);
   // res.status(200).send(`${JSON.stringify(res)}`);
